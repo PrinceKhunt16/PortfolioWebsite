cf70a86a4fc120e7ff6f4bfcb71ac7436fcb9fe3
Prince Khunt <princekhuntdatastructure@gmail.com>
2025-03-24 16:40:38 +0530

    Fixed issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7D7YK2Q0GR"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-7D7YK2Q0GR');
+            `,
+          }}
+        />
+      </head>
       <body>
         <Layout>
           {children}
