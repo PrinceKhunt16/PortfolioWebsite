aa492e5015705e019ecda550c9afca9d1cc486ec
Prince Khunt <princekhuntdatastructure@gmail.com>
2025-03-24 16:33:03 +0530

    Added analytics

---
+++ src/app/_document.js
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-7D7YK2Q0GR"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-7D7YK2Q0GR');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
